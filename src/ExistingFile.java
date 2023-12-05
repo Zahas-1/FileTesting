d96b3b384d9c3a2613d759d1669955c63aa19601
Firestrike05 <127893806+Firestrike05@users.noreply.github.com>
2023-12-05 15:48:20 +0000

    Update ExistingFile.java

--- src/ExistingFile.java
+++ src/ExistingFile.java
@@ -3,7 +3,7 @@ import java.sql.SQLOutput;
 
 public class ExistingFile {
     public static void main(String[] args) {
-        File f = new File("C:\\Users\\Zaiyn\\Desktop\\Coding\\example.txt");
+        File f = new File("Desktop\\Coding\\example.txt");
         if(!f.exists()) {
             System.out.println("The file does not exist");
         } else System.out.println("File found!");
