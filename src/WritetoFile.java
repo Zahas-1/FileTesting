4e43a80bd31ef3623107bd075df32e8adf9eae91
Firestrike05 <127893806+Firestrike05@users.noreply.github.com>
2023-12-05 15:49:05 +0000

    Update WritetoFile.java

--- src/WritetoFile.java
+++ src/WritetoFile.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class WritetoFile {
     public static void main(String[] args) {
         try {
-            FileWriter myWriter = new FileWriter("C:\\Users\\Zaiyn\\Desktop\\Coding\\example2.txt");
+            FileWriter myWriter = new FileWriter("Desktop\\Coding\\example2.txt");
             myWriter.write("Files in Java might be tricky, but it is fun enough!");
             myWriter.close();
             System.out.println("Successfully wrote to the file");
